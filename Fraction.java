383cfdd82f6c384fa2e8861628a2e41c2a85d629
jakarid <97747027+jakarid@users.noreply.github.com>
2022-01-28 20:13:18 -0500

    Update Fraction.java

--- Fraction.java
+++ Fraction.java
@@ -4,18 +4,18 @@ public class Fraction
 {
 	public static void main(String[] args)
 	{
-    // declare a scanner object for input
+       // declare a scanner object for input
 		Scanner scan = new Scanner(System.in);
 
-    // prompt for numerator and read in - use print instead of println so the user will input on the same line as the prompt
-         System.out.println("Enter the numerator: ");
-         String name = scan.nextLine();
-         System.out.println("Enter the denominator: ");
-    // prompt for denomator and read in - use print instead of println so the user will input on the same line as the prompt
-    
-    // compute the answer and store in a variable of type double
-    
-    // ouutput the answer
-    
+       // prompt for numerator and read in - use print instead of println so the user will input on the same line as the prompt
+         System.out.print("Enter the numerator: ");
+         int numerator = scan.nextInt();
+       // prompt for denomator and read in - use print instead of println so the user will input on the same line as the prompt		
+         System.out.print("Enter the denominator: ");
+	 int denominator = scan.nextInt();
+       // compute the answer and store in a variable of type double
+	 double fraction = numerator / denominator;
+       // ouutput the answer		
+	 System.out.println(fraction);	  
   }
 }
